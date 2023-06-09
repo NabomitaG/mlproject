05b92e46464955965e50cfb7f6df6c9e2fec32fb
nabomita <nabomitaghoshal@yahoo.com>
2023-06-09 14:37:46 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,14 @@
 import sys
+import logging
 
 def error_message_Detail(error,error_detail:sys):
     _,_,exe_tb=error_detail.exc_info()
     file_name =exe_tb.tb_frame.f_code.co_filename
     error_message ="Error occured in python [{0}] line numb [{1}] error message [{2}]".format(
-        file_name,exe_tb.tb_lineno,str(error)
+        file_name,exe_tb.tb_lineno,str(error))
 
-        return error_message
-    )
+    return error_message
+    
 
     class CustomException(Exception):
         def __init__(self,error_message,error_detail:sys):
@@ -15,5 +16,12 @@ def error_message_Detail(error,error_detail:sys):
             self.error_message =error_message_Detail(error_message,error_detail=error_detail)
 
         def __str__(self):
-            return self.error_message        
+            return self.error_message 
+
+    if __name__=="__main__":
+        try:
+            a=1/0
+        except Exception as e:
+            logging.info("Divide by zero error logged")
+            raise CustomException(e,sys)       
     
